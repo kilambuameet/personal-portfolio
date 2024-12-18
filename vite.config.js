9ffde0a9e9a1d7ed67f8369ad5468a9b286c0e83
Amit Kilambu <amitkilambu8@gmail.com>
2024-12-18 18:13:32 +0545

    Fix deployment issues

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { ghPages } from 'vite-plugin-gh-pages';
 
 export default defineConfig({
   plugins: [react(), ghPages()],
-  base: '/your-repo-name/', // replace 'your-repo-name' with your actual GitHub repo name
+  base: '/Personal-Portfolio/', // replace 'your-repo-name' with your actual GitHub repo name
 });
