39f674ab8e15b40e4cf31d0a8fa4231f39c78859
Amit Kilambu <amitkilambu8@gmail.com>
2025-01-05 12:24:51 +0545

    changed the folder name

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { ghPages } from 'vite-plugin-gh-pages';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { ghPages } from "vite-plugin-gh-pages";
 
 export default defineConfig({
   plugins: [react(), ghPages()],
-  base: '/Personal-Portfolio/',  // Set this to the name of your repository
+  base: "/personal-portfolio/", // Set this to the name of your repository
   // Other Vite configurations
 });
